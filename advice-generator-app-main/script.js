5c8e1ef9af2f372130b35464e9ccbe3d11336a1d
Your Name <you@example.com>
2022-07-27 15:50:24 +0100

    first commit

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,9 @@
+// fetch('https://api.adviceslip.com/advice') 
+//     .then((response) => response.json())
+//     .then((data) => {
+//         let advice = data.advice
+
+//         const p = document.querySelector('.advice-text')
+//         p.textContent = advice
+//         console.log(advice)
+//     })
